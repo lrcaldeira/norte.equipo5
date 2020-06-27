5085d0b5f13570e08cb77e2bbea8ac9a8711186c
Luciana Rodriguez Caldeira <lurc85@gmail.com>
2020-06-27 18:18:20 -0300

    ABM Producto

--- norte.equipo5.Data/Model/Product.cs
+++ norte.equipo5.Data/Model/Product.cs
@@ -1,5 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +10,27 @@ namespace norte.equipo5.Data.Model
 {
    public class Product: IdentityBase
     {
+
+        [DisplayName("Titulo")]
         public string Title { get; set;}
+
+        [DisplayName("Descripción")]
         public string Description { get; set;}
+
+        [Required]
+        [DisplayName("Id Artista")]
         public int ArtistId { get; set;}
+
+        [DisplayName("Imagen")]
         public string Image { get; set;}
+
+        [DisplayName("Precio")]
         public double Price { get; set;}
+
+        [DisplayName("Cantidad Vendidos")]
         public int QuantitySold { get; set;}
+
+        [DisplayName("Calificación Promedio")]
         public double AvgStars { get; set; }
 
         public virtual Artist Artist { get; set; } //el artista que es el producto//
