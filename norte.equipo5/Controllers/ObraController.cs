0ff7200ad9ac4b2e38d0ddff98088b38d3b0501c
lucas_peron <lucas_peron@outlook.com.ar>
2020-06-29 18:06:47 -0300

    Últimos ajustes, GALERIA

---
+++ norte.equipo5/Controllers/ObraController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace norte.equipo5.Controllers
+{
+    public class ObraController : Controller
+    {
+        // GET: Obra
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
